a5136c57eb7285ab7f99599605ddc944a1e449b3
oyama919 <oyama919@gmail.com>
2017-10-04 22:43:42 +0900

    HTMLサニタイズ仕様確認

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
@@ -11,5 +12,17 @@ export class AppComponent  {
   book = {
     title: 'Book1',
     price: '3000'
+  };
+  safeMsg : SafeHtml
+  msg : string = `
+        <script>window.alert("こっちはアラートでない");</script>
+        <p>HTML文書のエスケープ使用確認</p>
+        <a href="#" onclick=" alert('onclickのアラートでる')">リンク</a>
+        <button>「Buttonタグある」</button>
+        <font color="Red">インラインの色は出る</font>
+        `;
+  constructor(private sanitizer: DomSanitizer) {
+    // msgプロパティのサニタイズを許可
+    this.safeMsg = sanitizer.bypassSecurityTrustHtml(this.msg);
   }
 }
