8ae765ba01a8cf23f81a96022e49bbecddabd400
oyama919 <oyama919@gmail.com>
2017-10-04 22:14:05 +0900

    Book Component（仮）追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
 })
 
 export class AppComponent  {
   title = 'Angular';
   contnets = 'Angular Sample Contents'
+  book = {
+    title: 'Book1',
+    price: '3000'
+  }
 }
